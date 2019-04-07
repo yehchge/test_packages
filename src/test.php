a94221fd06fae54c70deff2b3231d2dce4fc6cd6
bill.yeh <bill.yeh@iwant-in.net>
2019-04-07 17:24:29 +0800

    change hello

--- src/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function get_file_ext($file){
-	return strtolower(pathinfo($file, PATHINFO_EXTENSION));
-}
-
---
+++ src/yehchge/hello.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yehchge;
+
+class hello {
+
+	public static function hello($file){
+		return "Hello World, Composer!";
+	}
+}
